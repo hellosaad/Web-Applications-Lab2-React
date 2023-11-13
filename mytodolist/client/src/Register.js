fe4441efa62bb58d694f209d5e401b866235e3d3
hellosaad <54971401+hellosaad@users.noreply.github.com>
2023-11-13 02:05:31 -0600

    register using useResource done

--- mytodolist/client/src/Register.js
+++ mytodolist/client/src/Register.js
@@ -1,14 +1,23 @@
 import React, { useState, useContext } from "react";
-import { StateContext } from "./contexts"; // Import the StateContext
+import { StateContext } from "./contexts";
+import { useResource } from "react-request-hook"; // Import the useResource hook
+import { useEffect } from "react";
 
 export default function Register() {
-  const { dispatch } = useContext(StateContext); 
-  const [username, setUsername] = useState("");
+  const { dispatch } = useContext(StateContext);
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [passwordRepeat, setpasswordRepeat] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
 
-  function handleUsername(evt) {
-    setUsername(evt.target.value);
+  // Define the useResource hook for POST to create a new user
+  const [userRegister, register] = useResource((email, password) => ({
+    url: "http://localhost:4000/users", // The endpoint for user registration
+    method: "post",
+    data: { email, password },
+  }));
+
+  function handleEmail(evt) {
+    setEmail(evt.target.value);
   }
 
   function handlePassword(evt) {
@@ -16,26 +25,46 @@ export default function Register() {
   }
 
   function handlePasswordRepeat(evt) {
-    setpasswordRepeat(evt.target.value);
+    setPasswordRepeat(evt.target.value);
   }
 
+ function handleRegister(e) {
+   e.preventDefault();
+   if (password !== passwordRepeat) {
+     alert("Passwords do not match");
+   } else {
+     // Call the register function directly
+     register(email, password);
+     // After calling register, the userRegister object will be updated with the response
+   }
+ }
+
+ // ... later in your component, where you can handle the result
+ useEffect(() => {
+   if (userRegister && userRegister.data) {
+     // Handle the successful registration
+     dispatch({ type: "REGISTER", username: email });
+     // Navigate the user to a different page or clear the form
+   }
+   // Handle errors if any
+   if (userRegister && userRegister.error) {
+     // Handle the registration error
+     console.error("Registration failed:", userRegister.error);
+   }
+ }, [userRegister, dispatch, email]);
+
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (password === passwordRepeat) {
-          dispatch({ type: "REGISTER", username });
-          alert("Passwords do not match");
-        }
-      }}
-    >
-      <label htmlFor="register-username">Username:</label>
+    <form onSubmit={handleRegister}>
+
+      <label htmlFor="register-email">Email:</label>
       <input
-        type="text"
-        value={username}
-        onChange={handleUsername}
-        name="register-username"
-        id="register-username"
+        type="email"
+        value={email}
+        onChange={handleEmail}
+        name="register-email"
+        id="register-email"
+        required
       />
 
       <label htmlFor="register-password">Password:</label>
@@ -60,7 +89,7 @@ export default function Register() {
         type="submit"
         value="Register"
         disabled={
-          username.length === 0 ||
+          email.length === 0 ||
           password.length === 0 ||
           password !== passwordRepeat
         }
